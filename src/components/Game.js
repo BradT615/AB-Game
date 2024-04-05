de3f5e6b9ebda56aed08de0ecbb7dfd05e7e7db6
BradT615 <125227539+BradT615@users.noreply.github.com>
2024-04-05 03:37:02 -0400

    refactored logic

--- src/components/Game.js
+++ src/components/Game.js
@@ -7,7 +7,6 @@ function Game() {
   const [guess, setGuess] = useState(['', '', '', '']);
   const [hints, setHints] = useState([]);
   const [secretCode, setSecretCode] = useState('');
-  const [backspaceCount, setBackspaceCount] = useState([0, 0, 0, 0]);
   const [timer, setTimer] = useState(0);
   const [isPaused, setIsPaused] = useState(true);
   const [firstGuessEntered, setFirstGuessEntered] = useState(false);
@@ -16,19 +15,13 @@ function Game() {
   const [emptyFields, setEmptyFields] = useState([false, false, false, false]);
   const [borderColor, setBorderColor] = useState('border-gray-400');
 
-  const inputRef0 = useRef();
-  const inputRef1 = useRef();
-  const inputRef2 = useRef();
-  const inputRef3 = useRef();
-
-  const inputRefs = [inputRef0, inputRef1, inputRef2, inputRef3];
+  const inputRefs = [useRef(), useRef(), useRef(), useRef()];
+  const hintsEndRef = useRef(null);
 
   useEffect(() => {
     generateSecretCode();
     inputRefs[0].current.focus();
   }, []);
-
-  const hintsEndRef = useRef(null);
 
   useEffect(() => {
     if (hintsEndRef.current) {
@@ -44,8 +37,7 @@ function Game() {
       return () => clearInterval(interval);
     }
   }, [hints, isPaused]);
-  
-  
+
   const handlePause = () => {
     setIsPaused(!isPaused);
   };
@@ -57,31 +49,30 @@ function Game() {
   };
 
   const generateSecretCode = () => {
-    let code = '';
-    while (code.length < 4) {
-      let digit = Math.floor(Math.random() * 10);
-      if (!code.includes(digit)) {
-        code += digit;
-      }
-    }
+    let code = '1234';
+    // while (code.length < 4) {
+    //   let digit = Math.floor(Math.random() * 10);
+    //   if (!code.includes(digit)) {
+    //     code += digit;
+    //   }
+    // }
     setSecretCode(code);
   };
 
   const handleGuessChange = (index, event) => {
     if (gameWon) return;
-  
+
     let newGuess = [...guess];
     let input = event.target.value;
-  
-    // Check if the input is a number
+
     if (!isNaN(input)) {
       newGuess[index] = input;
       setGuess(newGuess);
-  
+
       let newEmptyFields = [...emptyFields];
       newEmptyFields[index] = false;
       setEmptyFields(newEmptyFields);
-  
+
       if (event.target.nextSibling) {
         event.target.nextSibling.focus();
       }
@@ -94,7 +85,7 @@ function Game() {
     if (event.key === "Backspace") {
       event.preventDefault();
       let newGuess = [...guess];
-  
+
       if (newGuess[index] !== '') {
         newGuess[index] = '';
       } else if (index > 0) {
@@ -103,26 +94,38 @@ function Game() {
           event.target.previousSibling.focus();
         }
       }
-  
+
       setGuess(newGuess);
     } else if (event.key === "Enter") {
       handleGuessSubmit();
-    } else {
-      let newBackspaceCount = [...backspaceCount];
-      newBackspaceCount[index] = 0;
-      setBackspaceCount(newBackspaceCount);
     }
   };
 
+  const checkBulls = (guess, secretCode) => {
+    return guess.filter((digit, index) => digit === secretCode[index]).length;
+  };
+
+  const checkCows = (guess, secretCode) => {
+    const secretCodeCopy = [...secretCode];
+    return guess.filter((digit, index) => {
+      if (digit === secretCode[index]) {
+        secretCodeCopy[index] = null;
+        return false;
+      }
+      const cowIndex = secretCodeCopy.indexOf(digit);
+      if (cowIndex !== -1) {
+        secretCodeCopy[cowIndex] = null;
+        return true;
+      }
+      return false;
+    }).length;
+  };
+
   const handleGuessSubmit = () => {
-    let aCount = 0;
-    let bCount = 0;
-    let secretCodeCopy = [...secretCode];
-  
     if (guess.join('').length !== 4) {
       let newEmptyFields = guess.map(g => g === '');
       setEmptyFields(newEmptyFields);
-      unfocusAll(); // Unfocus all input fields
+      unfocusAll();
       return;
     }
 
@@ -130,23 +133,12 @@ function Game() {
       setFirstGuessEntered(true);
       setIsPaused(false);
     }
-  
-    for (let i = 0; i < secretCode.length; i++) {
-      if (guess[i] === secretCode[i]) {
-        aCount++;
-        secretCodeCopy = secretCodeCopy.filter(digit => digit !== guess[i]);
-      }
-    }
-  
-    for (let i = 0; i < secretCode.length; i++) {
-      if (guess[i] !== secretCode[i] && secretCodeCopy.includes(guess[i])) {
-        bCount++;
-        secretCodeCopy = secretCodeCopy.filter(digit => digit !== guess[i]);
-      }
-    }
-  
+
+    const aCount = checkBulls(guess, secretCode);
+    const bCount = checkCows(guess, secretCode);
+
     setHints([...hints, { guess: guess.join(''), hint: `${aCount}A${bCount}B` }]);
-  
+
     if (aCount === 4) {
       setIsPaused(true);
       setGameWon(true);
@@ -157,7 +149,7 @@ function Game() {
       inputRefs[0].current.focus();
     }
   };
-  
+
   const unfocusAll = () => {
     inputRefs.forEach(ref => {
       if (ref.current) {
@@ -198,10 +190,12 @@ function Game() {
           </div>
           {firstGuessEntered && (
             <div className='flex items-center gap-2'>
-              <h1 className='h-full no-select'>{isPaused ? formatTime(timer) : formatTime(timer)}</h1>
-              <button onClick={handlePause} className='mt-1 md:mt-0 text-3xl sm:text-4xl transition hover:scale-110 hover:text-zinc-50'>
-                {isPaused ? <RiPlayMiniFill /> : <RiPauseMiniLine />}
-              </button>
+              <h1 className='h-full no-select'>{formatTime(timer)}</h1>
+              {!gameWon && (
+                <button onClick={handlePause} className='mt-1 md:mt-0 text-3xl sm:text-4xl transition hover:scale-110 hover:text-zinc-50'>
+                  {isPaused ? <RiPlayMiniFill /> : <RiPauseMiniLine />}
+                </button>
+              )}
             </div>
           )}
         </div>
@@ -221,8 +215,8 @@ function Game() {
             />
           ))}
           <h1 className='hidden lg:block text-lg mt-2'>Press the <b className='border-b-[1px]'>Enter</b> key or <br/> <b className='border-b-[1px]'>Submit</b> button to Guess</h1>
-          <button 
-            className={`border-2 ${guess.join('').length == 4 && !gameWon ? 'border-accent' : 'border-gray-300'} w-2/3 mx-auto mt-2 lg:mt-8 py-1 sm:py-2 rounded-lg ${gameWon ? 'bg-zinc-700 border-gray-400 text-zinc-200' : 'hover:bg-zinc-500 hover:border-gray-200 hover:text-zinc-100'}`} 
+          <button
+            className={`border-2 ${guess.join('').length == 4 && !gameWon ? 'border-accent' : 'border-gray-300'} w-2/3 mx-auto mt-2 lg:mt-8 py-1 sm:py-2 rounded-lg ${gameWon ? 'bg-zinc-700 border-gray-400 text-zinc-200' : 'hover:bg-zinc-500 hover:border-gray-200 hover:text-zinc-100'}`}
             onClick={handleGuessSubmit}
             disabled={gameWon}
           >
@@ -231,7 +225,7 @@ function Game() {
         </div>
         <div className='flex justify-between gap-4 w-full items-center lg:mb-6 mt-2'>
           <button className='border-2 border-gray-300 w-1/2 py-1 sm:py-2 rounded-lg hover:bg-zinc-500 hover:border-gray-200 hover:text-zinc-100' onClick={handleReveal}>Reveal</button>
-          <button className='border-2 border-gray-300 w-1/2 py-1 sm:py-2 rounded-lg hover:bg-zinc-500 hover:border-gray-200 hover:text-zinc-100' onClick={handleNewGame}>New Game</button>
+          <button className={`border-2 ${gameWon ? 'border-accent hover:bg-zinc-500 hover:border-accent hover:text-zinc-100' : 'border-gray-300 hover:bg-zinc-500 hover:border-gray-200 hover:text-zinc-100'} w-1/2 py-1 sm:py-2 rounded-lg`} onClick={handleNewGame}>New Game</button>
         </div>
       </div>
       <div className='flex flex-col justify-between p-2 sm:p-4 rounded-lg w-full h-full bg-zinc-700 bg-opacity-90 backdrop-blur-sm'>
@@ -239,7 +233,7 @@ function Game() {
           Hints <span className='float-right'>Guess Count: {hints.length}</span>
         </h1>
         <div className='text-xl sm:text-3xl text-left p-4 overflow-auto h-full max-h-[50vh] lg:max-h-full'>
-          {!isPaused && firstGuessEntered && (
+          {firstGuessEntered && (
             <table className='w-full text-center'>
               <thead>
                 <tr>
